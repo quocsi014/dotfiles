3b02ea82496cb5293ea668d913c71522774dfc68
quocsi014 <quocsi014@gmail.com>
2024-12-30 23:59:40 +0700

    feat hide image background

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,37 +1,51 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup({})
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+				settings = {
+					tailwindCSS = {
+						experimental = {
+							classRegex = {
+								{ "clsx\\(([^)]*)\\)", "([^\\s]+)" },
+								{ "classnames\\(([^)]*)\\)", "([^\\s]+)" },
+							},
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local os = require("os")
 
 local config = wezterm.config_builder()
@@ -9,28 +9,44 @@ config.window_decorations = "RESIZE"
 -- image setting
 local user_home = os.getenv("HOME")
 local background_folder = user_home .. "/.config/bg"
-config.window_background_image = background_folder..'/6678293.jpg'
+local current_background_image = background_folder .. "/6678293.jpg"
+config.window_background_image = nil
 config.window_background_image_hsb = {
-  hue = 1.0,
-  saturation = 4,
-  brightness = 0.2,
+	hue = 1.0,
+	saturation = 4,
+	brightness = 0.2,
 }
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  bottom = 0,
-  top = 0,
+	left = 0,
+	right = 0,
+	bottom = 0,
+	top = 0,
 }
 config.font_size = 14
-config.font =
-  wezterm.font('JetBrains Mono', {
-    weight = 'Medium',
-  })
-config.color_scheme = 'Tokyo Night Storm'
+config.font = wezterm.font("JetBrains Mono", {
+	weight = "Medium",
+})
+config.color_scheme = "Tokyo Night Storm"
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
+--key map
+config.keys = {
+	{
+		key = "b",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action_callback(function(window)
+			local overrides = window:get_config_overrides() or {}
+			if not overrides.window_background_image then
+				overrides.window_background_image = current_background_image
+			else
+				overrides.window_background_image = nil
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
 return config
