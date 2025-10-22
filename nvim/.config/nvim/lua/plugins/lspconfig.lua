a2e41d9a40f6a672d4fccf25887bf88a1f566f69
quocsi014 <quocsi014@gmail.com>
2025-10-23 01:17:15 +0700

    update

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,5 @@
 return {
+  -- Mason: cài đặt LSP servers, formatters, linters
   {
     "williamboman/mason.nvim",
     lazy = false,
@@ -6,6 +7,8 @@ return {
       require("mason").setup({})
     end,
   },
+
+  -- Mason-LSPConfig: liên kết mason với nvim-lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
@@ -13,13 +16,16 @@ return {
       auto_install = true,
     },
   },
+
+  -- Main LSP setup
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
+      local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Setup các LSP servers với vim.lsp.config (Neovim 0.11+)
+      -- Danh sách servers
       local servers = {
         "ts_ls",
         "pylsp",
@@ -30,14 +36,15 @@ return {
         "sqlls",
       }
 
+      -- Setup từng server
       for _, server in ipairs(servers) do
-        vim.lsp.config(server, {
+        lspconfig[server].setup({
           capabilities = capabilities,
         })
       end
 
-      -- Setup Tailwind CSS với config đặc biệt
-      vim.lsp.config("tailwindcss", {
+      -- Tailwind CSS setup riêng
+      lspconfig.tailwindcss.setup({
         capabilities = capabilities,
         filetypes = {
           "html",
@@ -52,7 +59,7 @@ return {
           tailwindCSS = {
             experimental = {
               classRegex = {
-                { "clsx\\(([^)]*)\\)",       "([^\\s]+)" },
+                { "clsx\\(([^)]*)\\)", "([^\\s]+)" },
                 { "classnames\\(([^)]*)\\)", "([^\\s]+)" },
               },
             },
@@ -60,40 +67,37 @@ return {
         },
       })
 
-      -- Enable LSP servers
-      for _, server in ipairs(servers) do
-        vim.lsp.enable(server)
-      end
-      vim.lsp.enable("tailwindcss")
-
       -- Keymaps cho LSP
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Show References" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+      local keymap = vim.keymap.set
+      keymap("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+      keymap("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      keymap("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
+      keymap("n", "<leader>gr", vim.lsp.buf.references, { desc = "Show References" })
+      keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+      keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
+      keymap("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
     end,
   },
+
+  -- Mason Tool Installer: đảm bảo tool được cài
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     lazy = false,
     config = function()
       require("mason-tool-installer").setup({
         ensure_installed = {
-          -- LSP
+          -- LSP servers
           "gopls",
           "typescript-language-server",
           "lua-language-server",
           "tailwindcss-language-server",
           "sqls",
           "buf-language-server",
-          -- Formatter
+          -- Formatters
           "prettier",
           "stylua",
           "clang-format",
-          -- Linter
+          -- Linters
           "eslint_d",
           "golangci-lint",
         },
